746538f023ee6a3ee476ac67d677818dabe58b01
yuya-katsuragi <yuya_katsuragi@asupage.co.jp>
2026-02-19 16:38:14 +0900

    add: Case01.java .java
    トップページURLでアクセスし、
    タイトルの値が期待値と一致するか確認するテストを作成。
    
    期待値は以下の通り
    タイトル：ログイン | LMS

--- test_code_creation_katsuragiyuya/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
+++ test_code_creation_katsuragiyuya/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,12 +10,14 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.boot.test.context.SpringBootTest;
 
 /**
  * 結合テスト ログイン機能①
  * ケース01
  * @author holy
  */
+@SpringBootTest
 @TestMethodOrder(OrderAnnotation.class)
 @DisplayName("ケース01 ログイン画面への遷移")
 public class Case01 {
@@ -30,7 +33,7 @@ public class Case01 {
 	static void after() {
 		closeDriver();
 	}
-	
+
 	/**
 	 * Case.1_1 ログインコントローラー試験　初期表示テスト(未ログイン時 正常終了)
 	 * ■対象メソッド：index() 
@@ -46,12 +49,13 @@ public class Case01 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
 		
+		goTo("http://localhost:8080/lms/");
 		
-		
-		
-		getEvidence(STR);
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+
+		getEvidence(new Object() {
+		}, "loginDisplay");
 	}
 
 }
