e64d1b8ad6ed8b50c7827aecc1fb7d18c2089106
yuya-katsuragi <yuya_katsuragi@asupage.co.jp>
2026-02-25 09:33:15 +0900

    add:Case06.java カテゴリ検索を行うテストコードを追加しました。
    カテゴリ検索し検索結果が該当カテゴリのみ表示される、
    画面下部のQを押下することでAが表示されるテストコードを追加しました。

--- test_code_creation_katsuragiyuya/src/test/java/jp/co/sss/lms/ct/f02_faq/Case06.java
+++ test_code_creation_katsuragiyuya/src/test/java/jp/co/sss/lms/ct/f02_faq/Case06.java
@@ -1,6 +1,9 @@
 package jp.co.sss.lms.ct.f02_faq;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +12,8 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト よくある質問機能
@@ -35,42 +40,95 @@ public class Case06 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		goTo("http://localhost:8080/lms/");
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+
+		getEvidence(new Object() {
+		}, "topPageAccess");
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		//loginメソッドを呼び出しid,password,ログインボタンを取得
+		login("StudentAA01", "StudentAA01a");
+		// 画面右上の「ようこそ」メッセージ要素を取得
+		WebElement welcomMsg = webDriver.findElement(By.xpath("//a[@href='/lms/user/detail']/small"));
+		// 取得したテキストが期待通り表示されているかを確認
+		assertEquals("ようこそ受講生ＡＡ１さん", welcomMsg.getText());
+
+		getEvidence(new Object() {
+		}, "login Successful");
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「ヘルプ」リンクからヘルプ画面に遷移")
 	void test03() {
-		// TODO ここに追加
+		//ドロップダウンメニューの親要素をクリックして、展開
+		webDriver.findElement(By.linkText("機能")).click();
+		//展開されたメニュー内のヘルプリンクをクリック
+		webDriver.findElement(By.xpath("//a[@href='/lms/help']")).click();
+		//遷移先のタイトルが期待通りで表示されているか確認
+		assertEquals("ヘルプ | LMS", webDriver.getTitle());
+
+		getEvidence(new Object() {
+		}, "helpScreen");
+
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「よくある質問」リンクからよくある質問画面を別タブに開く")
 	void test04() {
-		// TODO ここに追加
+		// 「よくある質問」のリンク要素を特定
+		WebElement commonQuestions = webDriver.findElement(By.xpath("//a[@href='/lms/faq']"));
+		// リンクをクリック（target="_blank" により新しいタブが開く）
+		commonQuestions.click();
+		// ウィンドウハンドルを取得
+		ArrayList<String> tabs = new ArrayList<>(webDriver.getWindowHandles());
+		// 新しいタブに切り替え
+		webDriver.switchTo().window(tabs.get(1));
+		assertEquals("よくある質問 | LMS", webDriver.getTitle());
+
+		getEvidence(new Object() {
+		}, "commonQuestions");
 	}
 
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 カテゴリ検索で該当カテゴリの検索結果だけ表示")
 	void test05() {
-		// TODO ここに追加
+		//linkTextでテキストと完全一致するリンクを検索
+		webDriver.findElement(By.linkText("【研修関係】")).click();
+		pageLoadTimeout(5);
+		//JSを実行し、ページの最下部までスクロール
+		scrollBy("document.body.scrollHeight");
+		//遷移先の検証
+		assertTrue(webDriver.getCurrentUrl().contains("frequentlyAskedQuestionCategoryId=1"));
+		getEvidence(new Object() {
+		}, "CategorySearch");
 	}
 
 	@Test
 	@Order(6)
 	@DisplayName("テスト06 検索結果の質問をクリックしその回答を表示")
 	void test06() {
-		// TODO ここに追加
-	}
+		//class="mb10" を持つ <dt> 要素を探してクリック
+		webDriver.findElement(By.cssSelector("dt.mb10")).click();
+		//JSを実行し、ページの最下部までスクロール
+		scrollBy("document.body.scrollHeight");
+		//期待値(テキスト)を定義
+		String answer = "受講者の退職や解雇等、やむを得ない事情による途中終了に関してなど、"
+				+ "事情をお伺いした上で、協議という形を取らせて頂きます。"
+				+ " 弊社営業担当までご相談下さい。";
+		//実際の回答要素の特定
+		WebElement searchAnswer = webDriver.findElement(By.cssSelector("dd.fs18>span:last-child"));
+		//内容の比較
+		assertEquals(answer, searchAnswer.getText());
+		getEvidence(new Object() {
+		}, "CategoryAnswer");
 
+	}
 }
