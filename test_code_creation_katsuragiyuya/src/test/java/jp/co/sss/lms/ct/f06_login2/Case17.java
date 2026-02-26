cc523d8a166a359e9db42105a8e13f69649d0979
yuya-katsuragi <yuya_katsuragi@asupage.co.jp>
2026-02-26 11:04:54 +0900

    add:Case17.java 初回ログイン後セキュリティ規約とパスワード変更を正常に動作するか確認するテストコードを追加しました。
    初回ログイン後利用規約に同意し正しいパスワード変更を行い、コース詳細画面に遷移するか確認するテストコードを追加しました。
    
    【期待値】
    タイトル：コース詳細 | LMS

--- test_code_creation_katsuragiyuya/src/test/java/jp/co/sss/lms/ct/f06_login2/Case17.java
+++ test_code_creation_katsuragiyuya/src/test/java/jp/co/sss/lms/ct/f06_login2/Case17.java
@@ -1,6 +1,9 @@
 package jp.co.sss.lms.ct.f06_login2;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +12,8 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト ログイン機能②
@@ -35,28 +40,92 @@ public class Case17 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		goTo("http://localhost:8080/lms/");
+
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+
+		getEvidence(new Object() {
+		}, "topPageAccess");
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 DBに初期登録された未ログインの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		//loginメソッドを呼び出しid,password,ログインボタンを取得
+		login("StudentAA04", "StudentAA04");
+		// 取得したテキストが期待通り表示されているかを確認
+		assertEquals("セキュリティ規約 | LMS", webDriver.getTitle());
+
+		getEvidence(new Object() {
+		}, "firstlogin");
+
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 「同意します」チェックボックスにチェックを入れ「次へ」ボタン押下")
 	void test03() {
-		// TODO ここに追加
+		pageLoadTimeout(5);
+		//JSを実行し、ページの最下部までスクロール
+		scrollBy("document.body.scrollHeight");
+
+		//JSを実行し、ページの最下部までスクロール
+		scrollBy("document.body.scrollHeight");
+		//チェックボックスの要素を取得
+		WebElement checkBox = webDriver.findElement(By.name("securityFlg"));
+		//チェックボックスを押下
+		checkBox.click();
+		//内容を比較
+		assertEquals("セキュリティ規約 | LMS", webDriver.getTitle());
+		getEvidence(new Object() {
+		}, "SecuritySuccess");
+
+		//getEvidenceより後に押下することで正しいエビデンスを取得
+		webDriver.findElement(By.cssSelector("button.btn-primary")).click();
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 変更パスワードを入力し「変更」ボタン押下")
 	void test04() {
-		// TODO ここに追加
+
+		pageLoadTimeout(5);
+		visibilityTimeout(By.id("currentPassword"), 10);
+
+		String nowPassword = "StudentAA04";
+		String correctPassword = "StudentAA04a";
+
+		//パスワードを入力
+		webDriver.findElement(By.id("currentPassword")).sendKeys(nowPassword);
+		webDriver.findElement(By.id("password")).sendKeys(correctPassword);
+		webDriver.findElement(By.id("passwordConfirm")).sendKeys(correctPassword);
+
+		//JSを実行し、ページの最下部までスクロール
+		scrollBy("document.body.scrollHeight");
+
+		//入力したパスワードのエビデンスを取得
+		getEvidence(new Object() {
+		}, "passwordChange");
+
+		//変更ボタンを押下
+		webDriver.findElement(By.xpath("//button[text()='変更']")).click();
+
+		pageLoadTimeout(5);
+		//モーダルウィンドウの要素が出現するまで待機
+		visibilityTimeout(By.id("upd-btn"), 5);
+		//モーダルウィンドウの更新ボタンの要素を取得
+		WebElement updateButton = webDriver.findElement(By.id("upd-btn"));
+		//モーダルウィンドウの更新ボタンを押下
+		updateButton.click();
+		// 画面右上の「ようこそ」メッセージ要素を取得
+		WebElement welcomMsg = webDriver.findElement(By.xpath("//a[@href='/lms/user/detail']/small"));
+		// 取得したテキストが期待通り表示されているかを確認
+		assertThat(welcomMsg.getText(), containsString("ようこそ受講生ＡＡ４さん"));
+
+		//正しくパスワードできた場合詳細画面に遷移するため遷移後のエビデンスを取得
+		getEvidence(new Object() {
+		}, "passwordChangeSuccessful");
 	}
 
 }
