33f4b1318b282596e72c0f2686049e3d6db956eb
yuya-katsuragi <yuya_katsuragi@asupage.co.jp>
2026-02-26 10:39:20 +0900

    add:Case15.java  初回ログイン時のセキュリティ規約画面のテストコードを追加しました。
    初回ログインのユーザーでログインし、利用規約画面でチェックせずに「次に」を押すと期待通りのエラーが表示されるか確認するテストコード追加しました

--- test_code_creation_katsuragiyuya/src/test/java/jp/co/sss/lms/ct/f06_login2/Case15.java
+++ test_code_creation_katsuragiyuya/src/test/java/jp/co/sss/lms/ct/f06_login2/Case15.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f06_login2;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,8 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト ログイン機能②
@@ -35,21 +38,47 @@ public class Case15 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		goTo("http://localhost:8080/lms/");
+
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+
+		getEvidence(new Object() {
+		}, "topPageAccess");
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 DBに初期登録された未ログインの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		//loginメソッドを呼び出しid,password,ログインボタンを取得
+		login("StudentAA02", "StudentAA02");
+		// 取得したテキストが期待通り表示されているかを確認
+		assertEquals("セキュリティ規約 | LMS", webDriver.getTitle());
+
+		getEvidence(new Object() {
+		}, "firstlogin");
+
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 「同意します」チェックボックスにチェックをせず「次へ」ボタンを押下")
 	void test03() {
-		// TODO ここに追加
+		//JSを実行し、ページの最下部までスクロール
+		scrollBy("document.body.scrollHeight");
+		//次へを押下
+		webDriver.findElement(By.cssSelector("button.btn-primary")).click();
+		pageLoadTimeout(5);
+		//JSを実行し、ページの最下部までスクロール
+		scrollBy("document.body.scrollHeight");
+		//エラーメッセージを取得
+		WebElement errorMsg = webDriver.findElement(By.className("error"));
+		//内容の比較
+		assertEquals("セキュリティ規約への同意は必須です。", errorMsg.getText());
+
+		getEvidence(new Object() {
+		}, "SecurityError");
+
 	}
 
 }
