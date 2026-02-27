0c6b63e233259333e6e493a14fb0eb782dc971e7
yuya-katsuragi <yuya_katsuragi@asupage.co.jp>
2026-02-27 11:32:11 +0900

    add:Case07.java レポート新規登録と登録後ボタン名の変更が正しく行われるか確認するテストコードを追加しました。
    「未提出」ステータスの研修日を選択し、日報を提出後セクション詳細画面のボタン名が正しく更新されるか確認するテストコードを追加しました。

--- test_code_creation_katsuragiyuya/src/test/java/jp/co/sss/lms/ct/f03_report/Case07.java
+++ test_code_creation_katsuragiyuya/src/test/java/jp/co/sss/lms/ct/f03_report/Case07.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f03_report;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,8 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト レポート機能
@@ -35,35 +38,75 @@ public class Case07 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		goTo("http://localhost:8080/lms/");
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+
+		getEvidence(new Object() {
+		}, "topPageAccess");
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		//loginメソッドを呼び出しid,password,ログインボタンを取得
+		login("StudentAA01", "StudentAA01a");
+		// 画面右上の「ようこそ」メッセージ要素を取得
+		WebElement welcomMsg = webDriver.findElement(By.xpath("//a[@href='/lms/user/detail']/small"));
+		// 取得したテキストが期待通り表示されているかを確認
+		assertEquals("ようこそ受講生ＡＡ１さん", welcomMsg.getText());
+
+		getEvidence(new Object() {
+		}, "loginSuccessful");
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 未提出の研修日の「詳細」ボタンを押下しセクション詳細画面に遷移")
 	void test03() {
-		// TODO ここに追加
+
+		//300px分、下にスクロール
+		scrollBy("300");
+		// セクションIDが3のフォーム内にある送信ボタンを特定してクリック
+		webDriver.findElement(By.xpath("//form[.//input[@name='sectionId' and "
+				+ "@value='4']]//input[@type='submit']")).click();
+
+		//指定したタイトル文字列になるまで待機
+		titleTimeout("セクション詳細 | LMS", 5);
+
+		//内容の比較
+		assertEquals("セクション詳細 | LMS", webDriver.getTitle());
+
+		getEvidence(new Object() {
+		}, "notSubmitted");
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「提出する」ボタンを押下しレポート登録画面に遷移")
 	void test04() {
-		// TODO ここに追加
+		//日報【デモ】を提出するを押下
+		webDriver.findElement(By.cssSelector("input[value='日報【デモ】を提出する']")).click();
+		//指定したタイトル文字列になるまで待機
+		titleTimeout("レポート登録 | LMS", 5);
+		//内容の比較
+		assertEquals("レポート登録 | LMS", webDriver.getTitle());
+
+		getEvidence(new Object() {
+		}, "sectionScreen");
 	}
 
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 報告内容を入力して「提出する」ボタンを押下し確認ボタン名が更新される")
 	void test05() {
-		// TODO ここに追加
+		//指定したテキストに入力
+		webDriver.findElement(By.id("content_0")).sendKeys("明日も頑張ります！");
+		//提出ボタンを押下
+		webDriver.findElement(By.xpath("//button[text()='提出する']")).click();
+
+		getEvidence(new Object() {
+		}, "reportRegistration ");
 	}
 
 }
